2a3f8eae90a79dec754d8004d6738c82fc9b0751
ericdudley <me@ericdudley.com>
2021-10-24 15:49:55 -0700

    remove .js files

--- src/ts/index.js
+++
@@ -1,8 +0,0 @@
-import initHeaderAnimation from './header';
-import initStretch from './stretch';
-window.addEventListener('DOMContentLoaded', () => {
-    const noTransitionOnLoadElements = document.querySelectorAll('.no-transition-on-load');
-    noTransitionOnLoadElements.forEach((element) => element.classList.remove('no-transition-on-load'));
-    initHeaderAnimation();
-    initStretch();
-});
