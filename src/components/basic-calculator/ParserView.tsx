fc2079ea4e212c07d13930243492f53c873e9a12
Eric Dudley <17283326+ericdudley@users.noreply.github.com>
2023-09-03 18:34:04 -0700

    fix: In-line styles for Tree view

--- src/components/basic-calculator/ParserView.tsx
+++ src/components/basic-calculator/ParserView.tsx
@@ -1,14 +1,15 @@
+import { useColorMode } from '@docusaurus/theme-common';
 import React from 'react';
 import { AnimatedTree } from 'react-tree-graph';
 import 'react-tree-graph/dist/style.css';
 import AutoSizer from 'react-virtualized-auto-sizer';
-import styles from './ParserView.module.css';
 import { useBasicCalculatorStore } from '../../utils/basic-calculator/store';
 
 import StatusText from './StatusText';
 
 export default function ParserView() {
   const { treeData, error } = useBasicCalculatorStore((state) => state.getTreeViewData());
+  const { isDarkTheme } = useColorMode();
 
   return (
     <div className="min-h-[50vh] w-full">
@@ -26,13 +27,38 @@ export default function ParserView() {
               data={treeData}
               keyProp="key"
               textProps={{
-                className: styles.text,
+                style: {
+                  fontSize: '2em',
+                  transform: 'translate(-15px, 4px)',
+                  ...(isDarkTheme
+                    ? {
+                      stroke: 'white',
+                      fill: 'white',
+                    }
+                    : {
+                      stroke: 'black',
+                      fill: 'black',
+                    }),
+                },
               }}
               gProps={{
-                className: styles.node,
+                style: {
+                  fill: 'transparent',
+                  stroke: 'transparent',
+                },
               }}
               pathProps={{
-                className: styles.path,
+                style: {
+                  ...(isDarkTheme
+                    ? {
+                      stroke: '#666666',
+                      fill: '#222222',
+                    }
+                    : {
+                      stroke: '#cccccc',
+                      fill: '#eeeeee',
+                    }),
+                },
               }}
             />
           )}
